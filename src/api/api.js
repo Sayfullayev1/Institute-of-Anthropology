2e92a6f577de39007f6fdb2f7eac62c7f8c0cc2f
SayfullaevSirojiddin <-email@example.com>
2026-09-07 18:21:41 +0500

    S

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-// const api = 'https://anthropology.uz';
+const api = 'https://anthropology.uz';
 
-const api = 'http://localhost:3100'; // для локальной разработки — напрямую на локальный бэкенд
+// const api = 'http://localhost:3100'; // для локальной разработки — напрямую на локальный бэкенд
 
 
 // const api = 'https://milliy-arxeologiya-markazi-admin-api-1.onrender.com';
